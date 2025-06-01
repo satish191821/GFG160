6b9bb6415c92072b9bde3f77477a374bbd2e3703
satish sahu <159688963+satish191821@users.noreply.github.com>
2025-06-01 23:53:33 +0530

    reverse a LL

---
+++ Day_65.java
@@ -0,0 +1,31 @@
+// function Template for Java
+
+/* linked list node class:
+
+class Node {
+    int data;
+    Node next;
+    Node(int value) {
+        this.value = value;
+    }
+ 
+
+*/
+// reverse a LL 
+class Solution {
+    Node reverseList(Node head) {
+        
+       Node curr=head;
+       Node prev=null;
+       Node next;
+       while(curr!=null){
+           next=curr.next;
+           curr.next=prev;
+           prev=curr;
+           curr=next;
+       }
+       Node revHead=prev;
+       return revHead;
+    }
+}      
+
